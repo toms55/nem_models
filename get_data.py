0918c762449889b56416b39bcec1d97ee84c4351
toms55 <tom.storey25@gmail.com>
2025-07-28 11:50:22 +1000

    before merging files

--- get_data.py
+++ get_data.py
@@ -2,21 +2,27 @@ import nemosis
 
 print(nemosis.defaults.dynamic_tables) # check available dynamic_tables
 print("\nCOLS:\n")
-print(nemosis.defaults.table_columns['DISPATCHLOAD']) # check what columns a table has
+print(nemosis.defaults.table_columns['DISPATCH_UNIT_SCADA']) # check what columns a table has
 
-start_time = '2021/03/01 00:00:00'
-end_time = '2021/03/01 23:59:59'
+start_time = '2021/01/01 00:00:00'
+end_time = '2025/07/24 23:59:59'
+
 price_table = 'DISPATCHPRICE'
 load_table = 'DISPATCHLOAD'
-price_data_cache = '/home/tom/Documents/nem_honours/price_data/'
-load_data_cache = '/home/tom/Documents/nem_honours/load_data/'
+misc_table = 'DISPATCHREGIONSUM'
 
-# price_data = nemosis.dynamic_data_compiler(start_time, end_time, price_table, price_data_cache, fformat='csv')
+price_data_cache = '/home/tom/Documents/nem_models/price_data/'
+load_data_cache = '/home/tom/Documents/nem_models/load_data/'
+misc_data_cache = '/home/tom/Documents/nem_models/nem_data/' # for checking table contents
 
-load_data = nemosis.dynamic_data_compiler(start_time, end_time, load_table, load_data_cache, fformat='csv')
+price_data = nemosis.dynamic_data_compiler(start_time, end_time, price_table, price_data_cache, fformat='csv')
+
+# load_data = nemosis.dynamic_data_compiler(start_time, end_time, load_table, load_data_cache, fformat='csv')
 
 
-# price_data = nemosis.dynamic_data_compiler(start_time, end_time, table, raw_data_cache, filter_cols=['REGIONID'], filter_values=(['SA1', 'NSW1'],), fformat='csv', rebuild=True) # filter based on REGIONID
+# price_data = nemosis.dynamic_data_compiler(start_time, end_time, table, price_data_cache, filter_cols=['REGIONID'], filter_values=(['SA1', 'NSW1'],), fformat='csv', rebuild=True) # filter based on REGIONID
 
-# print(load_data.head())
-# print(load_data.info())
+misc_data = nemosis.dynamic_data_compiler(start_time, end_time, misc_table, misc_data_cache, fformat='csv', rebuild=True)
+
+print(misc_data.head())
+print(misc_data.info())
